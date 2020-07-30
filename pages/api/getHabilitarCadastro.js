b6ca4a7258ad57cdf40f37d7b91a23e860f6900b
Kleyton Leite <musicpteclados@gmail.com>
2020-07-30 14:25:49 -0300

    validaçõa na api de controlle  do numero maximo de inscritos

--- pages/api/getHabilitarCadastro.js
+++ pages/api/getHabilitarCadastro.js
@@ -13,17 +13,19 @@ const doc = new GoogleSpreadsheet('1lZX6QuETHFhiTojs4RK8EqR32V_i2E79_yIMeprJHIQ'
         await sheet.loadCells('A3:D3')
 
         const sheet2 = doc.sheetsByIndex[1]
-        await sheet2.loadCells('G2:G2')
+        await sheet2.loadCells('G2:H2')
                      
-        const registered = sheet2.getCell(1, 6)               
+        const registered = sheet2.getCell(1, 6)
+        const numeroMaxInscrito = sheet2.getCell(1, 7)               
         const habilitarCadastro = sheet.getCell(2, 0)
         const pegarTextoCadastro = sheet.getCell(2, 1)
         const habilitarTextoCadastroErro = sheet.getCell(2, 2)
         const pegarTextoCadastroErro = sheet.getCell(2, 3)
+        
 
        
         res.end(JSON.stringify({
-            showCadastro: habilitarCadastro.value === 'VERDADEIRO' && registered.value < 250,
+            showCadastro: habilitarCadastro.value === 'VERDADEIRO' && registered.value < numeroMaxInscrito.value,
             message: pegarTextoCadastro.value,
             habilitarTextoCadastroErro:habilitarTextoCadastroErro.value === 'VERDADEIRO',
             messageErro: pegarTextoCadastroErro.value,
